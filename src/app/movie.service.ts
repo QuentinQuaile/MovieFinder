d78e10dd7695909e400a9334ee70eb581b0d0b36
Quentin Quaile <s00199608@mail.itsligo.ie>
2021-03-05 12:58:46 +0000

    first commit

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -8,13 +8,16 @@ export class MovieService {
   constructor() { }
 
   movieList = [
-    {id: 1, title: 'Gladiator', year:'2000', director: 'Ridley Scott'},
-    {id: 2, title: 'Hereditary',year: '2018', director: 'Ari Aster'},
-    {id: 3, title: 'Joker', year: '2019', director: 'Todd Phillips'},
-    {id: 4, title: 'Bohemian Rhapsody',year:'2018',director: 'Bryan Singer & Dexter Fletcher'}
+    {title: 'Gladiator', year:'2000', director: 'Ridley Scott'},
+    {title: 'Hereditary',year: '2018', director: 'Ari Aster'},
+    {title: 'Joker', year: '2019', director: 'Todd Phillips'},
+    {title: 'Bohemian Rhapsody',year:'2018',director: 'Bryan Singer & Dexter Fletcher'}
   ];
   getMovies()
   {
     return this.movieList;
+  }
+  addMovie(movietitle:string,moviedirector:string, movieyear:string){
+    this.movieList.push({title:movietitle,director:moviedirector,year:movieyear});
   }
 }
